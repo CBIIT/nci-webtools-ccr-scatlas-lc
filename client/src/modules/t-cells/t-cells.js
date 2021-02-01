cfeb12a33184a6bfaf732c39d44609e21afcf6f1
Benjamin Chen <benchen4998@gmail.com>
2021-02-01 14:13:02 -0500

    add T Cell plot

--- client/src/modules/t-cells/t-cells.js
+++ client/src/modules/t-cells/t-cells.js
@@ -4,8 +4,48 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import Plot from 'react-plotly.js';
+import groupBy from 'lodash/groupBy'
+
+import { tCellQuery } from './t-cell.state';
 
 export default function TCells() {
+
+    const tCells = useRecoilValue(tCellQuery);
+
+    const defaultLayout = {
+        xaxis: {
+            title: 't-SNE 1',
+            showticklabels: false,
+            zeroline: false,
+            scaleanchor: 'y',
+        },
+        yaxis: {
+            title: 't-SNE 2',
+            showticklabels: false,
+            zeroline: false,
+        },
+        hovermode: 'closest',
+    };
+
+    function getTraces({ columns, records }, groupByColumn) {
+        const xIndex = columns.indexOf('x');
+        const yIndex = columns.indexOf('y');
+        const groupIndex = columns.indexOf(groupByColumn);
+        const groups = groupBy(records, groupIndex);
+
+        return Object.entries(groups).map(([key, values]) => ({
+            name: key,
+            x: values.map(v => v[xIndex]),
+            y: values.map(v => v[yIndex]),
+            mode: 'markers',
+            type: 'scattergl',
+            hoverinfo: 'name',
+            marker: { size: 4, opacity: 0.7 }
+        }));
+    }
+
     return (
         <Container className="py-4">
             <Card className="shadow">
@@ -17,7 +57,19 @@ export default function TCells() {
                 <Card.Body>
                     <Row>
                         <Col xl={4}>
-                            Plot 1
+                            <Plot
+                                data={getTraces(tCells, 'type')}
+                                layout={{
+                                    ...defaultLayout,
+                                    title: 'T Cells',
+                                    legend: {
+                                        title: { text: 'Case (click to toggle)', font: { size: 14 } }
+                                    },
+                                }}
+                                useResizeHandler
+                                className="w-100"
+                                style={{ height: '800px' }}
+                            /> 
                         </Col>
                         <Col xl={4}>
                             Plot 2
@@ -25,6 +77,7 @@ export default function TCells() {
                         <Col xl={4}>
                             Plot 3
                         </Col>
+
                     </Row>
                 </Card.Body>
             </Card>
