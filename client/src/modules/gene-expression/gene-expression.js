221f5c730434bf946956db6a40927ec699575147
Brian Park <park-brian@users.noreply.github.com>
2021-02-02 15:46:30 -0500

    add clear gene button

--- client/src/modules/gene-expression/gene-expression.js
+++ client/src/modules/gene-expression/gene-expression.js
@@ -1,11 +1,16 @@
 import { Suspense } from 'react';
+import { useRecoilState } from 'recoil';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import GeneExpressionPlots from './gene-expression-plots';
 import GeneExpressionPlotOptions from './gene-expression-plot-options';
 import GeneExpressionCounts from './gene-expression-counts';
+import { geneState } from './gene-expression.state';
 
 export default function GeneExpression() {
+    const [gene, setGene] = useRecoilState(geneState);
+
     return (
         <Container className="py-4">
 
@@ -19,6 +24,8 @@ export default function GeneExpression() {
                     <GeneExpressionPlotOptions />
                     <hr />
                     <Suspense fallback={<div>Loading...</div>}>
+
+                        {gene && <Button variant="primary" onClick={_ => setGene('')}>Clear Gene ({gene})</Button>}
                         <GeneExpressionPlots />
                     </Suspense>
                 </Card.Body>
