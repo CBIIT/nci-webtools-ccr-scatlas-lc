42e31f16e1f70fbd26763c8be00ec1b90af66efc
Benjamin Chen <benchen4998@gmail.com>
2021-02-02 16:37:50 -0500

    add plot 2 and 3

--- client/src/modules/t-cell/t-cell-plots.js
+++ client/src/modules/t-cell/t-cell-plots.js
@@ -8,22 +8,24 @@ import { useRecoilState, useRecoilValue } from 'recoil';
 import Plot from 'react-plotly.js';
 import groupBy from 'lodash/groupBy'
 
-import { tCellQuery, tCellCountQuery } from './t-cell.state';
+import { tCellQuery, cd4Query, cd8Query, tCellCountQuery } from './t-cell.state';
 
-export default function TCells() {
+export default function TCellsPlots() {
 
     const tCells = useRecoilValue(tCellQuery);
+    const cd4 = useRecoilValue(cd4Query);
+    const cd8 = useRecoilValue(cd8Query);
     const tCellCount = useRecoilValue(tCellCountQuery);
 
     const defaultLayout = {
         xaxis: {
-            title: 't-SNE 1',
+            title: 'Component 1',
             showticklabels: false,
             zeroline: false,
             scaleanchor: 'y',
         },
         yaxis: {
-            title: 't-SNE 2',
+            title: 'Component 2',
             showticklabels: false,
             zeroline: false,
         },
@@ -61,22 +63,50 @@ export default function TCells() {
                             <Plot
                                 data={getTraces(tCells, 'type')}
                                 layout={{
-                                    ...defaultLayout,
-                                    title: 'T Cells',
-                                    legend: {
-                                        title: { text: 'Case (click to toggle)', font: { size: 14 } }
+                                    xaxis: {
+                                        title: 't-SNE 1',
+                                        showticklabels: false,
+                                        zeroline: false,
+                                        scaleanchor: 'y',
                                     },
+                                    yaxis: {
+                                        title: 't-SNE 2',
+                                        showticklabels: false,
+                                        zeroline: false,
+                                    },
+                                    hovermode: 'closest',
+                                    title: `T Cells (n=${tCells.records.length})`,
                                 }}
                                 useResizeHandler
                                 className="w-100"
                                 style={{ height: '800px' }}
-                            /> 
+                            />
                         </Col>
                         <Col xl={4}>
-                            Plot 2
+                            <Plot
+                                data={getTraces(cd4, 'type')}
+                                layout={{
+                                    ...defaultLayout,
+                                    title: `CD4+ T Cells (n=${cd4.records.length})`,
+
+                                }}
+                                useResizeHandler
+                                className="w-100"
+                                style={{ height: '800px' }}
+                            />
                         </Col>
                         <Col xl={4}>
-                            Plot 3
+                            <Plot
+                                data={getTraces(cd8, 'type')}
+                                layout={{
+                                    ...defaultLayout,
+                                    title: `CD8+ T Cells (n=${cd8.records.length})`,
+
+                                }}
+                                useResizeHandler
+                                className="w-100"
+                                style={{ height: '800px' }}
+                            />
                         </Col>
 
                     </Row>
