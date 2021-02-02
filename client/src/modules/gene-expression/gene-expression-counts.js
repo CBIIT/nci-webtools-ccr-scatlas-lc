f5ff20b57d9fba43023514811235f588d32aee24
Brian Park <park-brian@users.noreply.github.com>
2021-02-02 15:44:55 -0500

    update gene on click

--- client/src/modules/gene-expression/gene-expression-counts.js
+++ client/src/modules/gene-expression/gene-expression-counts.js
@@ -1,18 +1,29 @@
 
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useRecoilState } from 'recoil';
 import BootstrapTable from 'react-bootstrap-table-next';
+import Button from 'react-bootstrap/Button';
 // import filterFactory, { numberFilter, textFilter } from 'react-bootstrap-table2-filter';
 import paginationFactory from 'react-bootstrap-table2-paginator';
-import { geneCountsQuery } from './gene-expression.state';
+import { 
+    geneCountsQuery,
+    geneState,
+} from './gene-expression.state';
 
 export default function GeneExpressionCounts() {
     const geneCounts = useRecoilValue(geneCountsQuery);
+    const [gene, setGene] = useRecoilState(geneState);
+
+    function updateGene(gene) {
+        window.scrollTo(0, 0);
+        setGene(gene);
+    }
 
     const columns = [
         {
             dataField: 'gene',
             text: 'Gene',
             sort: true,
+            formatter: cell => <Button variant="link" className="p-0" onClick={_ => updateGene(cell)}>{cell}</Button>
         },
         {
             dataField: 'malignant_cell_count',
