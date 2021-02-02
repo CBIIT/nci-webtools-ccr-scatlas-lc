6ac8ecbf671ad648a6737ee89aa111c100175cd5
Brian Park <park-brian@users.noreply.github.com>
2021-02-02 02:07:09 -0500

    add common plot functions

---
+++ client/src/services/plot.js
@@ -0,0 +1,36 @@
+import groupBy from 'lodash/groupBy';
+
+export function getGroupedTraces({ columns, records }, groupByColumn, marker) {
+    const xIndex = columns.indexOf('x');
+    const yIndex = columns.indexOf('y');
+    const groupIndex = columns.indexOf(groupByColumn);
+    const groups = groupBy(records, groupIndex);
+
+    return Object.entries(groups).map(([key, values]) => ({
+        name: key,
+        x: values.map(v => v[xIndex]),
+        y: values.map(v => v[yIndex]),
+        mode: 'markers',
+        type: 'scattergl',
+        hoverinfo: 'name',
+        marker,
+    }));
+}
+
+export function getContinuousTrace({ columns, records }, valueColumn, marker) {
+    const xIndex = columns.indexOf('x');
+    const yIndex = columns.indexOf('y');
+    const valueIndex = columns.indexOf(valueColumn);
+
+    return {
+        x: records.map(r => r[xIndex]),
+        y: records.map(r => r[yIndex]),
+        mode: 'markers',
+        type: 'scattergl',
+        hoverinfo: 'name',
+        marker: { 
+            color: records.map(r => r[valueIndex]),
+            ...marker,
+        }
+    };
+}
