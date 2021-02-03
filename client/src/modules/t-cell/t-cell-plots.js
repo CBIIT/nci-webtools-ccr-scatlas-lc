24149032c1f15e5a68b1cbc08513aa407e46b78c
Benjamin Chen <benchen4998@gmail.com>
2021-02-03 11:21:28 -0500

    add plots to view specific t cells

--- client/src/modules/t-cell/t-cell-plots.js
+++ client/src/modules/t-cell/t-cell-plots.js
@@ -8,15 +8,25 @@ import { useRecoilState, useRecoilValue } from 'recoil';
 import Plot from 'react-plotly.js';
 import groupBy from 'lodash/groupBy'
 
-import { tCellQuery, cd4Query, cd8Query, tCellCountQuery, markerConfigState } from './t-cell.state';
+import {
+    getContinuousTrace,
+    getGroupedTraces
+} from '../../services/plot';
+
+import { tCellQuery, cd4Query, cd8Query, markerConfigState, tCellState, tCellGeneExpressionQuery, cd4GeneExpressionQuery, cd8GeneExpressionQuery } from './t-cell.state';
 
 export default function TCellsPlots() {
 
-    
+
     const tCells = useRecoilValue(tCellQuery);
     const cd4 = useRecoilValue(cd4Query);
     const cd8 = useRecoilValue(cd8Query);
-    const {size, opacity} = useRecoilValue(markerConfigState);
+    const { size, opacity } = useRecoilValue(markerConfigState);
+    const gene = useRecoilValue(tCellState);
+
+    const tCellGeneExpression = useRecoilValue(tCellGeneExpressionQuery);
+    const cd4GeneExpression = useRecoilValue(cd4GeneExpressionQuery);
+    const cd8GeneExpression = useRecoilValue(cd8GeneExpressionQuery);
 
     const defaultLayout = {
         xaxis: {
@@ -33,77 +43,110 @@ export default function TCellsPlots() {
         hovermode: 'closest',
     };
 
-    function getTraces({ columns, records }, groupByColumn, marker) {
-        const xIndex = columns.indexOf('x');
-        const yIndex = columns.indexOf('y');
-        const groupIndex = columns.indexOf(groupByColumn);
-        const groups = groupBy(records, groupIndex);
 
-        return Object.entries(groups).map(([key, values]) => ({
-            name: key,
-            x: values.map(v => v[xIndex]),
-            y: values.map(v => v[yIndex]),
-            mode: 'markers',
-            type: 'scattergl',
-            hoverinfo: 'name',
-            marker
-        }));
-    }
-
-    return (
-
-        <Row>
-            <Col xl={4}>
+    return gene ?
+        <>
+            <Row>
+                <Col xl={4}>
+                    <Plot
+                        data={[getContinuousTrace(tCellGeneExpression, 'value', { size, opacity, showscale: true })]}
+                        layout={{
+                            xaxis: {
+                                title: 't-SNE 1',
+                                showticklabels: false,
+                                zeroline: false,
+                                scaleanchor: 'y',
+                            },
+                            yaxis: {
+                                title: 't-SNE 2',
+                                showticklabels: false,
+                                zeroline: false,
+                            },
+                            title: `T Cells: ${gene} (n=${tCellGeneExpression.records.length})`,
+                        }}
+                        useResizeHandler
+                        className="w-100"
+                        style={{ height: '800px' }}
+                    />
+                </Col>
+                <Col xl={4}>
+                    <Plot
+                        data={[getContinuousTrace(cd4GeneExpression, 'value', { size, opacity, showscale: true })]}
+                        layout={{
+                            ...defaultLayout,
+                            title: `CD4+ T Cells:  ${gene} (n=${cd4GeneExpression.records.length})`,
+                        }}
+                        useResizeHandler
+                        className="w-100"
+                        style={{ height: '800px' }}
+                    />
+                </Col>
+                <Col xl={4}>
                 <Plot
-                    data={getTraces(tCells, 'type', {size, opacity})}
-                    layout={{
-                        xaxis: {
-                            title: 't-SNE 1',
-                            showticklabels: false,
-                            zeroline: false,
-                            scaleanchor: 'y',
-                        },
-                        yaxis: {
-                            title: 't-SNE 2',
-                            showticklabels: false,
-                            zeroline: false,
-                        },
-                        hovermode: 'closest',
-                        title: `T Cells (n=${tCells.records.length})`,
-                    }}
-                    useResizeHandler
-                    className="w-100"
-                    style={{ height: '800px' }}
-                />
-            </Col>
-            <Col xl={4}>
-                <Plot
-                    data={getTraces(cd4, 'type', {size, opacity})}
-                    layout={{
-                        ...defaultLayout,
-                        title: `CD4+ T Cells (n=${cd4.records.length})`,
+                        data={[getContinuousTrace(cd8GeneExpression, 'value', { size, opacity, showscale: true })]}
+                        layout={{
+                            ...defaultLayout,
+                            title: `CD8+ T Cells:  ${gene} (n=${cd8GeneExpression.records.length})`,
+                        }}
+                        useResizeHandler
+                        className="w-100"
+                        style={{ height: '800px' }}
+                    />
+                </Col>
+            </Row>
+        </>
+        :
+        <>
+            <Row>
+                <Col xl={4}>
+                    <Plot
+                        data={getGroupedTraces(tCells, 'type', { size, opacity })}
+                        layout={{
+                            xaxis: {
+                                title: 't-SNE 1',
+                                showticklabels: false,
+                                zeroline: false,
+                                scaleanchor: 'y',
+                            },
+                            yaxis: {
+                                title: 't-SNE 2',
+                                showticklabels: false,
+                                zeroline: false,
+                            },
+                            hovermode: 'closest',
+                            title: `T Cells (n=${tCells.records.length})`,
+                        }}
+                        useResizeHandler
+                        className="w-100"
+                        style={{ height: '800px' }}
+                    />
+                </Col>
+                <Col xl={4}>
+                    <Plot
+                        data={getGroupedTraces(cd4, 'type', { size, opacity })}
+                        layout={{
+                            ...defaultLayout,
+                            title: `CD4+ T Cells (n=${cd4.records.length})`,
 
-                    }}
-                    useResizeHandler
-                    className="w-100"
-                    style={{ height: '800px' }}
-                />
-            </Col>
-            <Col xl={4}>
-                <Plot
-                    data={getTraces(cd8, 'type', {size, opacity})}
-                    layout={{
-                        ...defaultLayout,
-                        title: `CD8+ T Cells (n=${cd8.records.length})`,
+                        }}
+                        useResizeHandler
+                        className="w-100"
+                        style={{ height: '800px' }}
+                    />
+                </Col>
+                <Col xl={4}>
+                    <Plot
+                        data={getGroupedTraces(cd8, 'type', { size, opacity })}
+                        layout={{
+                            ...defaultLayout,
+                            title: `CD8+ T Cells (n=${cd8.records.length})`,
 
-                    }}
-                    useResizeHandler
-                    className="w-100"
-                    style={{ height: '800px' }}
-                />
-            </Col>
-
-        </Row>
-
-    );
+                        }}
+                        useResizeHandler
+                        className="w-100"
+                        style={{ height: '800px' }}
+                    />
+                </Col>
+            </Row>
+        </>
 }
