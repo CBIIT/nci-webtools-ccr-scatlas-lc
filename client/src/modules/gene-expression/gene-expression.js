f786bb94e3f440fc3522e0f2440e743b9907efe8
Brian Park <park-brian@users.noreply.github.com>
2021-02-02 20:49:20 -0500

    use loader as suspense fallback

--- client/src/modules/gene-expression/gene-expression.js
+++ client/src/modules/gene-expression/gene-expression.js
@@ -3,6 +3,7 @@ import { useRecoilState } from 'recoil';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Spinner from 'react-bootstrap/Spinner';
 import GeneExpressionPlots from './gene-expression-plots';
 import GeneExpressionPlotOptions from './gene-expression-plot-options';
 import GeneExpressionCounts from './gene-expression-counts';
@@ -11,9 +12,15 @@ import { geneState } from './gene-expression.state';
 export default function GeneExpression() {
     const [gene, setGene] = useRecoilState(geneState);
 
+    const Loader = ({message}) => (
+        <div className="loader">
+            <Spinner variant="primary" animation="border" role="status" />
+            <div>{message || 'Loading'}</div>
+        </div>
+    );
+
     return (
         <Container className="py-4">
-
             <Card className="shadow mb-4">
                 <Card.Header className="bg-primary text-white d-flex justify-content-between align-items-center">
                     <Card.Title className="my-1">
@@ -23,12 +30,15 @@ export default function GeneExpression() {
                         Clear Gene ({gene})
                     </Button>}
                 </Card.Header>
-                <Card.Body>
+                <Card.Body className="position-relative">
                     <GeneExpressionPlotOptions />
                     <hr />
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <GeneExpressionPlots />
-                    </Suspense>
+                    
+                    <div style={{minHeight: '800px'}}>
+                        <Suspense fallback={<Loader message="Loading Plots" />}>
+                            <GeneExpressionPlots />
+                        </Suspense>
+                    </div>
                 </Card.Body>
             </Card>
 
@@ -38,8 +48,8 @@ export default function GeneExpression() {
                         Cell Counts
                     </Card.Title>
                 </Card.Header>
-                <Card.Body className="p-0">
-                    <Suspense fallback={<div>Loading...</div>}>
+                <Card.Body className="p-0" style={{minHeight: '600px'}}>
+                    <Suspense fallback={<Loader message="Loading Cell Counts" />}>
                         <GeneExpressionCounts />
                     </Suspense>
                 </Card.Body>
