970cd72336bb3bcb178b1d79b155cecc4f39ba98
Brian Park <park-brian@users.noreply.github.com>
2021-02-03 14:37:14 -0500

    adjust legends and titles

--- client/src/modules/t-cell/t-cell-plots.js
+++ client/src/modules/t-cell/t-cell-plots.js
@@ -40,6 +40,10 @@ export default function TCellsPlots() {
             showticklabels: false,
             zeroline: false,
         },
+        legend: {
+            itemsizing: 'constant',
+            itemwidth: 40,
+        },
         hovermode: 'closest',
     };
 
@@ -62,7 +66,7 @@ export default function TCellsPlots() {
                                 showticklabels: false,
                                 zeroline: false,
                             },
-                            title: `T Cells: ${gene} (n=${tCellGeneExpression.records.length})`,
+                            title: `<b>T Cells: ${gene} (n=${tCellGeneExpression.records.length})</b>`,
                         }}
                         useResizeHandler
                         className="w-100"
@@ -74,7 +78,7 @@ export default function TCellsPlots() {
                         data={[getContinuousTrace(cd4GeneExpression, 'value', { size, opacity, showscale: true })]}
                         layout={{
                             ...defaultLayout,
-                            title: `CD4+ T Cells:  ${gene} (n=${cd4GeneExpression.records.length})`,
+                            title: `<b>CD4+ T Cells:  ${gene} (n=${cd4GeneExpression.records.length})</b>`,
                         }}
                         useResizeHandler
                         className="w-100"
@@ -86,7 +90,7 @@ export default function TCellsPlots() {
                         data={[getContinuousTrace(cd8GeneExpression, 'value', { size, opacity, showscale: true })]}
                         layout={{
                             ...defaultLayout,
-                            title: `CD8+ T Cells:  ${gene} (n=${cd8GeneExpression.records.length})`,
+                            title: `<b>CD8+ T Cells:  ${gene} (n=${cd8GeneExpression.records.length})</b>`,
                         }}
                         useResizeHandler
                         className="w-100"
@@ -102,19 +106,20 @@ export default function TCellsPlots() {
                     <Plot
                         data={getGroupedTraces(tCells, 'type', { size, opacity })}
                         layout={{
+                            ...defaultLayout,
+                            title: `<b>T Cells (n=${tCells.records.length})</b>`,
+                            legend: {
+                                ...defaultLayout.legend,
+                                title: { text: 'Type (click to toggle)', font: { size: 14 } },
+                            },
                             xaxis: {
+                                ...defaultLayout.xaxis,
                                 title: 't-SNE 1',
-                                showticklabels: false,
-                                zeroline: false,
-                                scaleanchor: 'y',
                             },
                             yaxis: {
+                                ...defaultLayout.yaxis,
                                 title: 't-SNE 2',
-                                showticklabels: false,
-                                zeroline: false,
                             },
-                            hovermode: 'closest',
-                            title: `T Cells (n=${tCells.records.length})`,
                         }}
                         useResizeHandler
                         className="w-100"
@@ -126,8 +131,11 @@ export default function TCellsPlots() {
                         data={getGroupedTraces(cd4, 'type', { size, opacity })}
                         layout={{
                             ...defaultLayout,
-                            title: `CD4+ T Cells (n=${cd4.records.length})`,
-
+                            title: `<b>CD4+ T Cells (n=${cd4.records.length})</b>`,
+                            legend: {
+                                ...defaultLayout.legend,
+                                title: { text: 'Type (click to toggle)', font: { size: 14 } },
+                            },
                         }}
                         useResizeHandler
                         className="w-100"
@@ -139,8 +147,11 @@ export default function TCellsPlots() {
                         data={getGroupedTraces(cd8, 'type', { size, opacity })}
                         layout={{
                             ...defaultLayout,
-                            title: `CD8+ T Cells (n=${cd8.records.length})`,
-
+                            title: `<b>CD8+ T Cells (n=${cd8.records.length})</b>`,
+                            legend: {
+                                ...defaultLayout.legend,
+                                title: { text: 'Type (click to toggle)', font: { size: 14 } },
+                            },
                         }}
                         useResizeHandler
                         className="w-100"
