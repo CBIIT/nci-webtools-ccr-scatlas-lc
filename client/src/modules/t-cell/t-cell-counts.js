f28c7532e772982e032458adb64d4977f02adfd2
Benjamin Chen <benchen4998@gmail.com>
2021-02-03 10:20:52 -0500

    add T Cell Count Table

---
+++ client/src/modules/t-cell/t-cell-counts.js
@@ -0,0 +1,74 @@
+import { useMemo, useCallback } from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import Button from 'react-bootstrap/Button';
+import Table, { TextFilter, RangeFilter } from '../components/table';
+import { tCellCountQuery , tCellState } from './t-cell.state';
+
+export default function TCellCounts() {
+    const geneCounts = useRecoilValue(tCellCountQuery);
+    const setGene = useSetRecoilState(tCellState);
+    const updateGene = useCallback(gene => {
+        window.scrollTo(0, 0);
+        setGene(gene);
+    }, [setGene]);
+
+    const columns = useMemo(_ => [
+        {
+            accessor: 'gene',
+            Header: 'Gene',
+            Filter: TextFilter,
+            Cell: ({ value }) => (
+                <Button variant="link" className="p-0" onClick={_ => updateGene(value)}>
+                    {value}
+                </Button>
+            ),
+            placeholder: 'Enter gene',
+        },
+        {
+            Header: 'T Cells Expressing',
+            accessor: 't_cell_count',
+            Filter: RangeFilter,
+            filter: 'between',
+            minPlaceholder: 'Enter min value',
+            maxPlaceholder: 'Enter max value',
+        },
+        {
+            Header: 'CD4+ T Cells Expressing',
+            accessor: 'cd4_cell_count',
+            Filter: RangeFilter,
+            filter: 'between',
+            minPlaceholder: 'Enter min value',
+            maxPlaceholder: 'Enter max value',
+        },
+        {
+            Header: 'CD8+ T Cells Expressing',
+            accessor: 'cd8_cell_count',
+            Filter: RangeFilter,
+            filter: 'between',
+            minPlaceholder: 'Enter min value',
+            maxPlaceholder: 'Enter max value',
+        }
+    ], [updateGene]);
+
+    const data = useMemo(_ =>
+        geneCounts.records,
+        [geneCounts]
+    );
+
+    const sortBy = useMemo(_ => [
+        { id: 't_cell_count', desc: true }
+    ], []);
+
+    const options = {
+        initialState: { sortBy },
+        defaultCanSort: true,
+    };
+
+    return (
+        <Table 
+            columns={columns} 
+            data={data} 
+            options={options} 
+        />
+    );
+}
--- client/src/modules/t-cell/t-cell.js
+++ client/src/modules/t-cell/t-cell.js
@@ -3,9 +3,11 @@ import { useRecoilState } from 'recoil';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Loader from '../components/loader';
 import TCellsPlots from './t-cell-plots';
 import TCellPlotOptions from './t-cell-plot-options';
 import { tCellState } from './t-cell.state';
+import TCellCounts from './t-cell-counts';
 
 export default function TCell() {
     const [tCell, settCell] = useRecoilState(tCellState);
@@ -25,11 +27,26 @@ export default function TCell() {
                 <Card.Body>
                     <TCellPlotOptions />
                     <hr />
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <TCellsPlots />
+                    <div style={{ minHeight: '800px' }}>
+                        <Suspense fallback={<Loader message="Loading Plots" />}>
+                            <TCellsPlots />
+                        </Suspense>
+                    </div>
+                </Card.Body>
+            </Card>
+            <Card className="shadow mb-4">
+                <Card.Header className="bg-primary text-white">
+                    <Card.Title className="my-1">
+                        T Cell Counts
+                    </Card.Title>
+                </Card.Header>
+                <Card.Body className="p-0 position-relative" style={{minHeight: '600px'}}>
+                    <Suspense fallback={<Loader message="Loading Cell Counts" />}>
+                        <TCellCounts />
                     </Suspense>
                 </Card.Body>
             </Card>
+
         </Container>
     );
 }
