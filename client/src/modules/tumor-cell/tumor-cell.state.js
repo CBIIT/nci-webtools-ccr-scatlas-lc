a89a23e3c3b415e14a4d4003606c776af36f7467
Brian Park <park-brian@users.noreply.github.com>
2021-02-05 13:42:53 -0500

    rename gene-expression to tumor-cell

--- client/src/modules/tumor-cell/tumor-cell.state.js
+++ client/src/modules/tumor-cell/tumor-cell.state.js
@@ -2,39 +2,39 @@ import { atom, selector } from 'recoil';
 import { query } from '../../services/query';
 
 export const malignantCellsQuery = selector({
-    key: 'geneExpression.malignantCellsQuery',
+    key: 'tumorCell.malignantCellsQuery',
     get: ({get}) => query('/api/query', {table: 'malignant_cell', raw: true}),
 });
 
 export const nonmalignantCellsQuery = selector({
-    key: 'geneExpression.nonmalignantCellsQuery',
+    key: 'tumorCell.nonmalignantCellsQuery',
     get: ({get}) => query('/api/query', {table: 'nonmalignant_cell', raw: true}),
 });
 
 export const geneCountsQuery = selector({
-    key: 'geneExpression.geneCountsQuery',
+    key: 'tumorCell.geneCountsQuery',
     get: async ({get}) => await query('/api/query', {table: `v_malignant_nonmalignant_cell_gene_count`, orderBy: 'malignant_cell_count', order: 'desc'}),
 });
 
 export const geneState = atom({
-    key: 'geneExpression.geneState',
+    key: 'tumorCell.geneState',
     default: '',
 });
 
 export const markerConfigState = atom({
-    key: 'geneExpression.markerConfigState',
+    key: 'tumorCell.markerConfigState',
     default: { size: 4, opacity: 0.8 },
 });
 
 export const malignantCellsGeneExpressionQuery = selector({
-    key: 'geneExpression.malignantCellsGeneExpressionQuery',
+    key: 'tumorCell.malignantCellsGeneExpressionQuer',
     get: async ({get}) => get(geneState) 
         ? await query('/api/query', {table: `v_malignant_cell_gene_expression_${get(geneState)}`, raw: true}) 
         : [],
 });
 
 export const nonmalignantCellsGeneExpressionQuery = selector({
-    key: 'geneExpression.nonmalignantCellsGeneExpressionQuery',
+    key: 'tumorCell.nonmalignantCellsGeneExpressionQuery',
     get: async ({get}) => get(geneState) 
         ? await query('/api/query', {table: `v_nonmalignant_cell_gene_expression_${get(geneState)}`, raw: true}) 
         : [],
