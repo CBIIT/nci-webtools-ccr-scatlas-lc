8fb9e2c3fa2ab83dce93cf043ac564b3a0e191ad
Benjamin Chen <benchen4998@gmail.com>
2021-02-05 15:36:50 -0500

    remvoe header from tumor and t cell pages

--- client/src/modules/t-cell/t-cell.js
+++ client/src/modules/t-cell/t-cell.js
@@ -1,7 +1,6 @@
 import { Suspense } from 'react'
 import { useRecoilState } from 'recoil';
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Loader from '../components/loader';
 import TCellsPlots from './t-cell-plots';
@@ -10,20 +9,11 @@ import { tCellState } from './t-cell.state';
 import TCellCounts from './t-cell-counts';
 
 export default function TCell() {
-    const [tCell, settCell] = useRecoilState(tCellState);
-
     return (
         <Container className="py-4">
+
             <Card className="shadow mb-4">
-                <Card.Header className="bg-primary text-white d-flex justify-content-between align-items-center">
-                    <Card.Title className="my-1">
-                        T Cells
-                    </Card.Title>
-                    {tCell && <Button variant="light" size="sm" onClick={_ => settCell('')}>
-                        Clear Gene ({tCell})
-                    </Button>}
-                </Card.Header>
-                <Card.Body className="position-relative">
+                <Card.Body>
                     <TCellPlotOptions />
                     <hr />
                     <div style={{ minHeight: '800px' }}>
