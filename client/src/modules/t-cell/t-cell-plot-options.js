a1d0f226bccc1f7d2482c610506f0b6b1d5233cc
Brian Park <park-brian@users.noreply.github.com>
2021-02-08 16:17:26 -0500

    allow users to enter more values for plot options

--- client/src/modules/t-cell/t-cell-plot-options.js
+++ client/src/modules/t-cell/t-cell-plot-options.js
@@ -1,14 +1,15 @@
+import { useState } from 'react';
 import { useRecoilState } from 'recoil';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button'
-import { markerConfigState } from './t-cell.state';
-import { geneState } from './t-cell.state';
+import Button from 'react-bootstrap/Button';
+import { markerConfigState, geneState } from './t-cell.state';
 
 export default function TCellPlotOptions() {
     const [markerConfig, setMarkerConfig] = useRecoilState(markerConfigState);
-    const [tCell, settCell] = useRecoilState(geneState);
+    const [config, setConfig] = useState(markerConfig);
+    const [gene, setGene] = useRecoilState(geneState);
 
     function handleChange(event) {
         const { name, value } = event.target;
@@ -17,28 +18,34 @@ export default function TCellPlotOptions() {
             opacity: [0.1, 1],
         }[name];
         const clampedValue = Math.min(max, Math.max(min, value));
+        setConfig({ ...config, [name]: value });
         setMarkerConfig({ ...markerConfig, [name]: clampedValue });
+    }
+
+    function handleBlur() {
+        setConfig(markerConfig);
     }
 
     return (
         <Row as={Form}>
             <Col md={3}>
-                <Form.Group>
+                <Form.Group controlId="cell-size">
                     <Form.Label>Cell Size</Form.Label>
-                    <Form.Control type="number" name="size" value={markerConfig.size} onChange={handleChange} min="1" max="10" />
+                    <Form.Control type="number" name="size" value={config.size} onChange={handleChange} onBlur={handleBlur} min="1" max="10" />
                 </Form.Group>
             </Col>
             <Col md={3}>
-                <Form.Group>
+                <Form.Group controlId="cell-opacity">
                     <Form.Label>Cell Opacity</Form.Label>
-                    <Form.Control type="number" name="opacity" value={markerConfig.opacity} onChange={handleChange} step="0.1" max="1" min="0.1" />
+                    <Form.Control type="number" name="opacity" value={config.opacity} onChange={handleChange} onBlur={handleBlur} step="0.1" max="1" min="0.1" />
                 </Form.Group>
             </Col>
+
             <Col md={6}>
                 <div className='float-right'>
                     <span>&nbsp;</span>
-                    {tCell && <Button variant="primary form-control mt-2" size="sm" onClick={_ => settCell('')}>
-                        Clear Gene ({tCell})
+                    {gene && <Button variant="primary form-control mt-2" size="sm" onClick={_ => setGene('')}>
+                        Clear Gene ({gene})
                 </Button>}
                 </div>
             </Col>
--- client/src/modules/tumor-cell/tumor-cell-plot-options.js
+++ client/src/modules/tumor-cell/tumor-cell-plot-options.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useRecoilState } from 'recoil';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
@@ -8,6 +9,7 @@ import { geneState } from './tumor-cell.state';
 
 export default function TumorCellPlotOptions() {
     const [markerConfig, setMarkerConfig] = useRecoilState(markerConfigState);
+    const [config, setConfig] = useState(markerConfig);
     const [gene, setGene] = useRecoilState(geneState);
 
     function handleChange(event) {
@@ -17,7 +19,12 @@ export default function TumorCellPlotOptions() {
             opacity: [0.1, 1],
         }[name];
         const clampedValue = Math.min(max, Math.max(min, value));
+        setConfig({ ...config, [name]: value });
         setMarkerConfig({ ...markerConfig, [name]: clampedValue });
+    }
+
+    function handleBlur() {
+        setConfig(markerConfig);
     }
 
     return (
@@ -25,13 +32,13 @@ export default function TumorCellPlotOptions() {
             <Col md={3}>
                 <Form.Group controlId="cell-size">
                     <Form.Label>Cell Size</Form.Label>
-                    <Form.Control type="number" name="size" value={markerConfig.size} onChange={handleChange} min="1" max="10" />
+                    <Form.Control type="number" name="size" value={config.size} onChange={handleChange} onBlur={handleBlur} min="1" max="10" />
                 </Form.Group>
             </Col>
             <Col md={3}>
                 <Form.Group controlId="cell-opacity">
                     <Form.Label>Cell Opacity</Form.Label>
-                    <Form.Control type="number" name="opacity" value={markerConfig.opacity} onChange={handleChange} step="0.1" max="1" min="0.1" />
+                    <Form.Control type="number" name="opacity" value={config.opacity} onChange={handleChange} onBlur={handleBlur} step="0.1" max="1" min="0.1" />
                 </Form.Group>
             </Col>
 
