6ac5f4fd83e3542ff68a06abf5bb0cae6f8e2ea2
Brian Park <park-brian@users.noreply.github.com>
2021-02-08 18:36:35 -0500

    handle empty parameters

--- client/src/services/query.js
+++ client/src/services/query.js
@@ -1,11 +1,13 @@
 export function asQueryParams(params) {
-    return '?' + Object.entries(params)
-        .map(([key, value]) => 
-            ![null, undefined, ''].includes(value) &&
-            [key, value].map(encodeURIComponent).join('=')
-        )
-        .filter(Boolean)
-        .join('&');
+    return params
+        ? '?' + Object.entries(params)
+            .map(([key, value]) => 
+                ![null, undefined, ''].includes(value) &&
+                [key, value].map(encodeURIComponent).join('=')
+            )
+            .filter(Boolean)
+            .join('&')
+        : '';
 }
 
 export async function query(url, params, options) {
