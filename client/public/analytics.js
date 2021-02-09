898dba7ffb44be6f6d0db75d0c885348e9ea833d
Brian Park <park-brian@users.noreply.github.com>
2021-02-09 13:40:20 -0500

    add analytics

---
+++ client/public/analytics.js
@@ -0,0 +1,12 @@
+(function () {
+    // Google Analytics snippet
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-62346354-27');
+
+    // Adobe DAP snippet
+    document.addEventListener('DOMContentLoaded', function() {
+        _satellite.pageBottom();
+    });
+})();
