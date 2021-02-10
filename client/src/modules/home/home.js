04da029b5fa1b3c413886b2535950cca8895cda0
Benjamin Chen <benchen4998@gmail.com>
2021-02-10 15:28:57 -0500

    update home page and site description

--- client/src/modules/home/home.js
+++ client/src/modules/home/home.js
@@ -19,11 +19,15 @@ export default function Home() {
                         <Col md={6}>
                             <h3>About scAtlasLC</h3>
                             <p>
-                                scAtlasLC (Single-cell Atlas in Liver Cancer) is a publicly available data portal of single-cell transcriptomic profiles of tumor cell communities in hepatocellular carcinoma and intrahepatic cholangiocarcinoma
+                                scAtlasLC (single-cell Atlas in Liver Cancer) is a publicly available data portal of single-cell transcriptomic profiles of tumor cell communities in hepatocellular carcinoma and intrahepatic cholangiocarcinoma
                             </p>
 
                             <p>
                                 scAtlasLC can be used to evaluate gene expression in malignant cells and various non-malignant cells in liver cancer. It can be further used to determine gene expression in different subtypes of stromal cells and immune cells.
+                            </p>
+
+                            <p>
+                                The single-cell data used for scAtlasLC can be downloaded from Gene Expression Omnibus: <a href="https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE151530" target="_blank">GSE151530</a>
                             </p>
                         </Col>
                     </Row>
