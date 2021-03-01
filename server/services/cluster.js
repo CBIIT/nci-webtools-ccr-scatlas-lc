7bd1fced9b18b99f4d46e81e1e6b679ce0a5c810
Brian Park <park-brian@users.noreply.github.com>
2021-03-01 13:37:45 -0500

    add cluster documentation

--- server/services/cluster.js
+++ server/services/cluster.js
@@ -1,13 +1,17 @@
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 
-function forkCluster(numProcesses, restartOnExit) {
+/**
+ * Forks the currently running script - if called within a forked process, returns false
+ * @param {number} numProcesses - determines number of processes to fork to (defaults to number of cpus)
+ * @param {boolean} restartOnExit - restart child processes if they stop for any reason (eg: on unhandled exceptions)
+ * @returns boolean - true if called from master process, false if called from child process (can be used for early termination 
+ * when in master process, eg: `if (forkCluster()) return;`)
+ */
+function forkCluster(numProcesses = numCPUs, restartOnExit = false) {
     if (!cluster.isMaster)
         return false;
-
-    if (!numProcesses)
-        numProcesses = numCPUs;
-    
+        
     for (let i = 0; i < numProcesses; i++)
         cluster.fork();
 
