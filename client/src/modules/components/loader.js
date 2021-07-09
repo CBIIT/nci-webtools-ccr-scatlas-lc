4ad11783d5cc12d060fe67e9c36173ef0d8f9dec
Brian Park <park-brian@users.noreply.github.com>
2021-07-09 00:44:50 -0400

    use overly-center class

--- client/src/modules/components/loader.js
+++ client/src/modules/components/loader.js
@@ -1,10 +1,10 @@
-import Spinner from 'react-bootstrap/Spinner';
+import Spinner from "react-bootstrap/Spinner";
 
-export default function Loader({message}) {
-    return (
-        <div className="loader">
-            <Spinner variant="primary" animation="border" role="status" />
-            <div>{message || 'Loading'}</div>
-        </div>
-    );
+export default function Loader({ message }) {
+  return (
+    <div className="overlay-center">
+      <Spinner variant="primary" animation="border" role="status" />
+      <div>{message || "Loading"}</div>
+    </div>
+  );
 }
