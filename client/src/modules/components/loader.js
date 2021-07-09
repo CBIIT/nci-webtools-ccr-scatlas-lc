0d537fc1838318f6e61a26289ebd638572fb44fd
Brian Park <park-brian@users.noreply.github.com>
2021-07-09 12:27:27 -0400

    use bs5 classes for loader

--- client/src/modules/components/loader.js
+++ client/src/modules/components/loader.js
@@ -2,9 +2,11 @@ import Spinner from "react-bootstrap/Spinner";
 
 export default function Loader({ message }) {
   return (
-    <div className="overlay-center">
-      <Spinner variant="primary" animation="border" role="status" />
-      <div>{message || "Loading"}</div>
+    <div className="d-flex flex-column align-items-center justify-content-center w-100 h-100 bg-white o-75 position-absolute top-0 start-0">
+      <div className="text-center">
+        <Spinner variant="primary" animation="border" role="status" />
+        <div>{message || "Loading"}</div>
+      </div>
     </div>
   );
 }
