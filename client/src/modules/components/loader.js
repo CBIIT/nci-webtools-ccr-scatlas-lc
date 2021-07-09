03363d3deb1e5d7d66649f87ff93a40061a9878f
Brian Park <park-brian@users.noreply.github.com>
2021-07-09 13:33:15 -0400

    add z-index to loader

--- client/src/modules/components/loader.js
+++ client/src/modules/components/loader.js
@@ -2,7 +2,9 @@ import Spinner from "react-bootstrap/Spinner";
 
 export default function Loader({ message }) {
   return (
-    <div className="d-flex flex-column align-items-center justify-content-center w-100 h-100 bg-white o-75 position-absolute top-0 start-0">
+    <div
+      className="d-flex flex-column align-items-center justify-content-center w-100 h-100 bg-white o-75 position-absolute top-0 start-0"
+      style={{ zIndex: 9999 }}>
       <div className="text-center">
         <Spinner variant="primary" animation="border" role="status" />
         <div>{message || "Loading"}</div>
