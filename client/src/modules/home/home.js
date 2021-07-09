e38cb9e6f3f1ed3e610c6fe8417fffbdd257c080
Brian Park <park-brian@users.noreply.github.com>
2021-07-09 00:40:44 -0400

    use svg component in home page

--- client/src/modules/home/home.js
+++ client/src/modules/home/home.js
@@ -1,39 +1,56 @@
-import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { ReactComponent as HomeImage } from "./home.svg";
 
 export default function Home() {
-    return (
-        <Container className="h-100">
-            <Card className="h-100 shadow rounded-0">
-                <Card.Body>
-                    <Row>
-                        <Col md={6}>
-                            <figure class="figure">
-                                <img src="images/home.svg" className="img-fluid" alt="Home Page Figure" />
-                                <figurecaption class="figure-caption">CAFs, cancer-associated fibroblasts; TAMs, tumor-associated macrophages; TECs, tumor-associated endothelial cells.</figurecaption>
-                            </figure>
-                        </Col>
+  return (
+    <Container className="h-100">
+      <Card className="h-100 shadow rounded-0">
+        <Card.Body>
+          <Row>
+            <Col md={6}>
+              <figure class="figure">
+                <HomeImage />
+                {/* <img src="images/home.svg" className="img-fluid" alt="Home Page Figure" /> */}
+                <figurecaption class="figure-caption">
+                  CAFs, cancer-associated fibroblasts; TAMs, tumor-associated
+                  macrophages; TECs, tumor-associated endothelial cells.
+                </figurecaption>
+              </figure>
+            </Col>
 
-                        <Col md={6}>
-                            <h3>About scAtlasLC</h3>
-                            <p>
-                                scAtlasLC (single-cell Atlas in Liver Cancer) is a publicly available data portal of single-cell transcriptomic profiles of tumor cell communities in hepatocellular carcinoma and intrahepatic cholangiocarcinoma.
-                            </p>
+            <Col md={6}>
+              <h1 className="h3">About scAtlasLC</h1>
+              <p>
+                scAtlasLC (single-cell Atlas in Liver Cancer) is a publicly
+                available data portal of single-cell transcriptomic profiles of
+                tumor cell communities in hepatocellular carcinoma and
+                intrahepatic cholangiocarcinoma.
+              </p>
 
-                            <p>
-                                scAtlasLC can be used to evaluate gene expression in malignant cells and various non-malignant cells in liver cancer. It can be further used to determine gene expression in different subtypes of stromal cells and immune cells.
-                            </p>
+              <p>
+                scAtlasLC can be used to evaluate gene expression in malignant
+                cells and various non-malignant cells in liver cancer. It can be
+                further used to determine gene expression in different subtypes
+                of stromal cells and immune cells.
+              </p>
 
-                            <p>
-                                The single-cell data used for scAtlasLC can be downloaded from Gene Expression Omnibus: <a href="https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE151530" target="_blank" rel="noopener noreferrer">GSE151530</a>
-                            </p>
-                        </Col>
-                    </Row>
-                </Card.Body>
-            </Card>
-        </Container>
-
-    );
+              <p>
+                The single-cell data used for scAtlasLC can be downloaded from
+                Gene Expression Omnibus:{" "}
+                <a
+                  href="https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE151530"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  GSE151530
+                </a>
+              </p>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
 }
