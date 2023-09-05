505669d76baff73494f20bd4d735c2e4aecbda68
Ben Chen <benchen4998@gmail.com>
2023-09-05 09:26:08 -0400

    add multiregional cohort components

---
+++ client/src/modules/multi-regional/multi-regional-counts.js
@@ -0,0 +1,67 @@
+import { useMemo, useCallback } from "react";
+import { useRecoilValue, useRecoilState } from "recoil";
+import Button from "react-bootstrap/Button";
+import Table, { TextFilter, RangeFilter } from "../components/table";
+import { tumorCellsStatsQuery, plotOptionsState , tumorCellsQuery} from "./multi-regional.state";
+
+export default function MultiRegionalCellCounts() {
+  const geneCounts = useRecoilValue(tumorCellsStatsQuery);
+  const [plotOptions, setPlotOptions] = useRecoilState(plotOptionsState);
+  const setGene = useCallback(
+    (gene) => {
+      window.scrollTo(0, 0);
+      setPlotOptions({ ...plotOptions, gene });
+    },
+    [plotOptions, setPlotOptions],
+  );
+  const tumorCell = useRecoilValue(tumorCellsQuery);
+
+  const columns = useMemo(
+    (_) => [
+      {
+        accessor: "gene",
+        Header: "Gene",
+        Filter: TextFilter,
+        Cell: ({ value }) => (
+          <Button
+            variant="link"
+            className="p-0"
+            onClick={(_) => setGene(value)}>
+            {value}
+          </Button>
+        ),
+        placeholder: "Enter gene",
+        aria: "Tumor Cell Gene",
+      },
+      {
+        Header: "Cells Expressing",
+        accessor: "count",
+        Filter: RangeFilter,
+        filter: "between",
+        minPlaceholder: "Enter min percent",
+        maxPlaceholder: "Enter max percent",
+        aria: "Tumor Cell Expressing",
+        Cell: ({ value }) => (
+          <span>
+            {(value / tumorCell.length*100).toFixed(1)}
+          </span>
+        ),
+      },
+    ],
+    [setGene],
+  );
+
+  const data = useMemo((_) => geneCounts, [geneCounts]);
+
+  const sortBy = useMemo(
+    (_) => [{ id: "gene", desc: false }],
+    [],
+  );
+
+  const options = {
+    initialState: { sortBy },
+    defaultCanSort: true,
+  };
+
+  return <Table columns={columns} data={data} options={options} />;
+}
---
+++ client/src/modules/multi-regional/multi-regional-plots.js
@@ -0,0 +1,165 @@
+import { useRecoilValue } from "recoil";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Plot from "react-plotly.js";
+import merge from "lodash/merge";
+import { getTraces } from "../../services/plot";
+import {
+  plotOptionsState,
+  tumorCellsQuery,
+  normalCellsQuery,
+  tumorGeneExpressionQuery,
+  normalGeneExpressionQuery,
+} from "./multi-regional.state";
+
+export default function MultiRegionalCellPlots() {
+  const tumorCells = useRecoilValue(tumorCellsQuery);
+  const normalCells = useRecoilValue(normalCellsQuery);
+  const { size, opacity, gene } = useRecoilValue(plotOptionsState);
+  const tumorGeneExpression = useRecoilValue(
+    tumorGeneExpressionQuery(gene),
+  );
+  const normalGeneExpression = useRecoilValue(
+    normalGeneExpressionQuery(gene),
+  );
+  const defaultLayout = {
+    xaxis: {
+      title: "t-SNE 1",
+      zeroline: false,
+      scaleanchor: "y",
+      scaleratio: 1,
+      constrain: "domain",
+    },
+    yaxis: {
+      title: "t-SNE 2",
+      zeroline: false,
+    },
+    legend: {
+      itemsizing: "constant",
+      itemwidth: 40,
+    },
+    hovermode: "closest",
+    uirevision: gene || 1,
+  };
+
+  const defaultConfig = {
+    displayModeBar: true,
+    toImageButtonOptions: {
+      format: "svg",
+      filename: "plot_export",
+      height: 1000,
+      width: 1000,
+      scale: 1,
+    },
+    displaylogo: false,
+    modeBarButtonsToRemove: [
+      "select2d",
+      "lasso2d",
+      "hoverCompareCartesian",
+      "hoverClosestCartesian",
+    ],
+  };
+
+  const traceColumns = {
+    groupColumn: "type",
+    valueColumn: gene,
+  };
+
+  const traceConfig = {
+    showlegend: !gene,
+    hoverinfo: !gene ? "name" : "text+name",
+    hoverlabel: {
+      namelength: -1,
+    },
+    marker: {
+      size,
+      opacity,
+      colorbar: {
+        thickness: 20,
+      },
+      ...(!gene && {
+        showscale: false,
+      }),
+    },
+  };
+
+  return gene ? (
+    <>
+      <Row>
+        <Col xl={6}>
+          <Plot
+            data={getTraces(
+              tumorGeneExpression,
+              traceConfig,
+              gene
+            )}
+            layout={merge({}, defaultLayout, {
+              title: `<b>Malignant Cells: ${gene} (n=${tumorGeneExpression.length})</b>`,
+            })}
+            config={defaultConfig}
+            useResizeHandler
+            className="w-100"
+            style={{ height: "800px" }}
+          />
+        </Col>
+        <Col xl={6}>
+          <Plot
+            data={getTraces(
+              normalGeneExpression,
+              traceConfig,
+              gene
+            )}
+            layout={merge({}, defaultLayout, {
+              title: `<b>Non-malignant Cells: ${gene} (n=${normalGeneExpression.length})</b>`,
+            })}
+            config={defaultConfig}
+            useResizeHandler
+            className="w-100"
+            style={{ height: "800px" }}
+          />
+        </Col>
+      </Row>
+    </>
+  ) : (
+    <>
+      <Row>
+        <Col xl={6}>
+          <Plot
+            data={getTraces(tumorCells, traceConfig, gene)}
+            layout={merge({}, defaultLayout, {
+              title: [
+                `<b>Malignant Cells (n=${tumorCells.length})</b>`,
+                `<span style="font-size: 12px; color: grey;">Click legend to show/hide cases</span>`,
+              ].join("<br>"),
+              legend: {
+                title: { text: "Case", font: { size: 14 } },
+              },
+            })}
+            config={defaultConfig}
+            useResizeHandler
+            className="w-100"
+            style={{ height: "800px" }}
+          />
+        </Col>
+        <Col xl={6}>
+          <Plot
+            data={getTraces(normalCells, traceConfig, gene)}
+            layout={merge({}, defaultLayout, {
+              title: [
+                `<b>Non-malignant Cells (n=${normalCells.length})</b>`,
+                '<span style="font-size: 12px; color: grey; ">Click legend to show/hide types</span>',
+              ].join("<br>"),
+              legend: {
+                title: { text: "Type", font: { size: 14 } },
+              },
+            })}
+            config={defaultConfig}
+            useResizeHandler
+            className="w-100"
+            style={{ height: "800px" }}
+          />
+        </Col>
+      </Row>
+    </>
+  );
+}
