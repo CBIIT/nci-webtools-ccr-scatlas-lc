d7f0255948ed66bcdbe9f2c742ad30fc6e5b2c18
Ben Chen <benchen4998@gmail.com>
2023-09-05 09:37:46 -0400

    add sequential page components

--- client/src/modules/pages/sequential.js
+++ client/src/modules/pages/sequential.js
@@ -1,6 +1,6 @@
-import TumorCell from "../tumor-cell/tumor-cell";
-import TCell from "../t-cell/t-cell";
-import { Container, Tabs, Tab } from "react-bootstrap";
+import SequentialCell from "../sequential/sequential";
+
+import { Container  } from "react-bootstrap";
 
 export default function Sequential() {
 
@@ -8,17 +8,7 @@ export default function Sequential() {
         <Container className="py-3">
             <h1 className="text-primary h3 mt-2">Sequential NCI-Clarity</h1>
             <hr />
-            <Tabs
-                defaultActiveKey="tumorCell"
-                className="mb-3"
-            >
-                <Tab eventKey="tumorCell" title="Tumor Cell Community">
-                    <TumorCell />
-                </Tab>
-                {/*<Tab eventKey="tCell" title="T-Cell">
-                    <TCell/>
-    </Tab>*/}
-            </Tabs>
+            <SequentialCell />
         </Container>
     )
 }
---
+++ client/src/modules/sequential/sequential.js
@@ -0,0 +1,54 @@
+import { Suspense } from "react";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Alert from "react-bootstrap/Alert";
+import Loader from "../components/loader";
+import ErrorBoundary from "../components/error-boundary";
+import SequentialCellPlots from "./sequential-plots";
+import SequentialCellPlotOptions from "./sequential-plot-options";
+import SequentialCellCounts from "./sequential-counts";
+
+export default function SequentialCell() {
+  return (
+    <Container>
+      <Card className="shadow mb-4">
+        <Card.Body className="position-relative" style={{ minHeight: "800px" }}>
+          <ErrorBoundary
+            fallback={
+              <Alert variant="danger">
+                An internal error prevented plots from loading. Please contact
+                the website administrator if this problem persists.
+              </Alert>
+            }>
+            <Suspense fallback={<Loader message="Loading Plots" />}>
+              <SequentialCellPlotOptions />
+              <hr />
+              <SequentialCellPlots />
+            </Suspense>
+          </ErrorBoundary>
+        </Card.Body>
+      </Card>
+
+      <Card className="shadow mb-4">
+        <Card.Header className="bg-primary text-white">
+          <Card.Title className="my-1">Cell Counts</Card.Title>
+        </Card.Header>
+        <Card.Body
+          className="p-0 position-relative"
+          style={{ minHeight: "600px" }}>
+          <ErrorBoundary
+            fallback={
+              <Alert variant="danger" className="m-3">
+                An internal error prevented cell counts from loading. Please
+                contact the website administrator if this problem persists.
+              </Alert>
+            }>
+            <Suspense fallback={<Loader message="Loading Cell Counts" />}>
+              <SequentialCellCounts />
+            </Suspense>
+          </ErrorBoundary>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+}
