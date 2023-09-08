d935bb24ddc6b8dd4274df26132bb70cd68c4dcb
Brian Park <park-brian@users.noreply.github.com>
2023-09-08 16:48:09 -0400

    move getSchema and add documentation

--- server/services/query.js
+++ server/services/query.js
@@ -1,159 +1,65 @@
+import { promisify } from "util";
 
-import util from "util";
-import getLogger from "./logger.js";
-const logger = getLogger("scatlas-lc");
+/**
+ * Quote a string for use in a SQL query
+ * @param {string} str
+ * @returns
+ */
+export function quote(str) {
+  return `"${str.replace('"', '\\"')}"`;
+}
 
-async function validate(schema, dataset, columns) {
-
-  if (!schema[dataset] || !Array.isArray(schema[dataset])) {
-    throw new Error(`Invalid dataset`);
+/**
+ * Return a schema object for the given database
+ * @param {import("duckdb").Database} db
+ * @returns {Promise<{[key: string]: any[]}>
+ */
+export async function getSchema(db) {
+  const run = promisify(db.all.bind(db));
+  const schema = {};
+  const tables = await run(
+    "select table_name as name from information_schema.tables",
+  );
+  for (const { name } of tables) {
+    const columns = await run(
+      "select column_name as name from information_schema.columns where table_name = ?",
+      [name],
+    );
+    schema[name] = columns.map((column) => column.name);
   }
-  const validColumns = columns.filter(c => schema[dataset].some(s => s.column_name === c));
+  return schema;
+}
+
+/**
+ * Filters/validates table and columns against the schema
+ * @param {any} schema
+ * @param {string} dataset
+ * @param {string[]} columns
+ * @returns {{dataset: string, columns: string[]}
+ */
+export function validate(schema, table, columns) {
+  if (!schema[table] || !Array.isArray(schema[table])) {
+    throw new Error(`Invalid table`);
+  }
+  const validColumns = columns.filter((column) =>
+    schema[table].some((validColumn) => column === validColumn),
+  );
   if (!validColumns.length) {
     throw new Error(`Invalid columns`);
   }
-  return { dataset, columns: validColumns };
+  return { table, columns: validColumns };
 }
 
-export async function query(database, schema, dataset, columns) {
-  
-  const run = util.promisify(database.all.bind(database));
-  const valid = await validate(schema, dataset, columns);
-  const sql = `select ${valid.columns.map(c => `"${c}"`).join(', ')} from "${valid.dataset}"`;
-  logger.info(sql)
-  return await run(sql);
-  /*
-  // enable read-only mode
-  database.pragma("query_only = ON");
-  const ifDefined = (value, statement, defaultValue = "") =>
-    value ? statement : defaultValue;
-  let { table, offset, limit, order, orderBy, columns, distinct, count, raw } =
-    params;
-
-  // validate provided table
-  const isValidTable =
-    database
-      .prepare(
-        `SELECT COUNT(*) FROM sqlite_master
-        WHERE tbl_name = :table`,
-      )
-      .pluck()
-      .get({ table }) > 0;
-
-  if (!table || !isValidTable) throw new Error("Please provide a valid table");
-
-  // retrieve column metadata
-  const columnNames = database
-    .prepare(`pragma table_info('${table}')`)
-    .all()
-    .map((c) => c.name);
-
-  // determine filters (eg: _column:eq=value)
-  const filters = Object.entries(params)
-    .filter(([key]) => key.startsWith("_"))
-    .map(([_key, value]) => {
-      // {'column:filter_type': value}
-      const [key, type] = _key.split(":");
-      return [key.replace(/^_/, ""), value, type || "eq"];
-    })
-    .filter(([key]) => columnNames.includes(key));
-
-  // map filters to an object containing {placeholder: value} props
-  let queryParams = {};
-  filters.forEach(([key, value, type]) => {
-    if (type === "like") {
-      queryParams[`${key}_${type}`] = `%${value}%`;
-    } else if (["in", "between"].includes(type)) {
-      value
-        .split(",")
-        .forEach((val, i) => (queryParams[`${key}_${type}_${i}`] = val));
-    } else {
-      queryParams[`${key}_${type}`] = value;
-    }
-  });
-
-  // columns, table, order, and orderBy must be sanitized/validated
-  // since they can not be bound parameters
-  columns = !columns
-    ? columnNames
-    : columns
-        .split(",")
-        .map((s) => s.trim())
-        .filter((column) => columnNames.includes(column));
-
-  if (order && !/^(asc|desc)$/i.test(order)) order = "asc";
-
-  if (orderBy && !columnNames.includes(orderBy)) orderBy = columns[0];
-
-  let conditions = ifDefined(
-    filters.length,
-    `WHERE ${filters
-      .map(
-        ([key, value, type]) => `
-                ${key} 
-                ${
-                  {
-                    // operator
-                    like: "LIKE",
-                    between: "BETWEEN",
-                    in: "IN",
-                    eq: "=",
-                    gt: ">",
-                    gte: ">=",
-                    lt: "<",
-                    lte: "<=",
-                  }[type] || "="
-                } 
-                ${
-                  {
-                    // placeholder
-                    in: `(${value
-                      .split(",")
-                      .map((_, i) => `:${key}_${type}_${i}`)})`,
-                    between: value
-                      .split(",")
-                      .map((_, i) => `:${key}_${type}_${i}`)
-                      .join(" AND "),
-                  }[type] || `:${key}_${type}`
-                }
-            `,
-      )
-      .join(" AND ")}`,
-  );
-
-  const queryColumns = columns.map((c) => `"${c}"`).join(",");
-
-  const statement = database.prepare(
-    `SELECT ${ifDefined(distinct, "DISTINCT")} ${queryColumns}
-        FROM "${table}" ${conditions}
-        ${ifDefined(order && orderBy, `ORDER BY ${orderBy} ${order}`)} 
-        ${ifDefined(limit, `LIMIT :limit`)}
-        ${ifDefined(offset, `OFFSET :offset`)}`,
-  );
-
-  let result = {
-    orderBy,
-    order,
-    offset,
-    limit,
-    filters: filters
-      .map(([key, value, type]) => [`_${key}:${type}`, value])
-      .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {}),
-    columns: statement.columns().map((c) => c.name),
-    records: statement.raw(Boolean(raw)).all({ offset, limit, ...queryParams }),
-  };
-
-  if (count) {
-    result.count = database
-      .prepare(
-        `SELECT COUNT(*) FROM (
-                SELECT ${ifDefined(distinct, "DISTINCT")} ${queryColumns} 
-                FROM "${table}" ${conditions}
-            )`,
-      )
-      .pluck()
-      .get(queryParams);
-  }
-
-  return result;*/
+/**
+ * Generate an escaped SQL query string for the given table and columns
+ * @param {any} schema
+ * @param {string} table
+ * @param {string[]} columns
+ * @returns {string} An escaped SQL query string
+ */
+export function getQuery(schema, table, columns) {
+  const valid = validate(schema, table, columns);
+  const validColumns = valid.columns.map(quote).join(", ");
+  const validTable = quote(valid.table);
+  return `select ${validColumns} from ${validTable}`;
 }
