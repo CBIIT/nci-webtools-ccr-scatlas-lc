4a9486720f76e8ac0dd39911a1ffc71bb0fa9c52
Thuong Nguyen <6723699+nhoktenz@users.noreply.github.com>
2023-10-10 10:12:27 -0400

    make the dropdown open when user click  onto the input

--- client/src/modules/components/select.js
+++ client/src/modules/components/select.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useCombobox } from "downshift";
 import classNames from "classnames";
 
@@ -13,15 +13,19 @@ export default function Select({
   value,
 }) {
   const [inputItems, setInputItems] = useState(options);
+  const [isInputFocused, setIsInputFocused] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
 
   const {
-    isOpen,
     getLabelProps,
     getMenuProps,
     getInputProps,
     getComboboxProps,
     highlightedIndex,
     getItemProps,
+    selectItem,
+    setInputValue,
   } = useCombobox({
     items: inputItems,
     selectedItem: value,
@@ -31,34 +35,69 @@ export default function Select({
           option.toLowerCase().startsWith(inputValue.toLowerCase()),
         ),
       );
+      setInputValue(inputValue);
+      setIsOpen(!!inputValue); // Close the dropdown when there's input
     },
-    onSelectedItemChange: ({ selectedItem, inputValue }) => {
+    // Add onSelectedItemChange here
+    onSelectedItemChange: ({ selectedItem }) => {
+      selectItem(selectedItem);
+      setIsInputFocused(false); // Set isInputFocused to false after selecting an item
+      setIsOpen(false); // Close the dropdown after selecting an item
       onChange(selectedItem);
     },
   });
+
+  const handleInputFocus = () => {
+    setIsInputFocused(true);
+    setIsOpen(true);
+  };
+
+  const handleInputBlur = () => {
+    setIsInputFocused(false);
+  };
+
+  const handleDropdownBlur = () => {
+    if (!isInputFocused) {
+      setIsOpen(false); // Close the dropdown when dropdown loses focus
+    }
+  };
+
+  console.log("isInputFocused -- ", isInputFocused);
+  console.log("isOpen ", isOpen);
 
   return (
     <>
       <label className="visually-hidden" {...getLabelProps()}>
         {label}
       </label>
-      <div className="w-100" {...getComboboxProps()}>
+      <div
+        className="w-100"
+        {...getComboboxProps()}
+        onBlur={handleDropdownBlur}>
         <input
           id={id}
           name={name}
           className={className}
           placeholder={placeholder}
+          onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
           {...getInputProps()}
         />
       </div>
       <div
+        ref={dropdownRef}
         className={classNames(
           "dropdown-menu",
           "overflow-scroll",
           "w-100",
-          isOpen && "show",
+          // isOpen && "show",
+          // Add "show" class when input is focused or user types
+          { show: isInputFocused || isOpen },
         )}
-        style={{ top: "40px", maxHeight: "200px" }}>
+        style={{ top: "40px", maxHeight: "200px" }}
+        tabIndex={-1} // Make the dropdown focusable
+        onBlur={handleDropdownBlur} // Handle blur event for dropdown
+      >
         <ul className="list-unstyled mb-0" {...getMenuProps()}>
           {isOpen && (
             <>
