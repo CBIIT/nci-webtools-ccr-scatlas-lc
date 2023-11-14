13a2b8b14d06c80b83c3e0c8987d7a24d1304ec2
Thuong Nguyen <6723699+nhoktenz@users.noreply.github.com>
2023-11-14 16:00:39 -0500

    clean up unused components

--- client/src/modules/about/about.js
+++ client/src/modules/about/about.js
@@ -1,8 +1,5 @@
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { Link } from "react-router-dom";
 import Table from "react-bootstrap/Table";
 
 export default function About() {
